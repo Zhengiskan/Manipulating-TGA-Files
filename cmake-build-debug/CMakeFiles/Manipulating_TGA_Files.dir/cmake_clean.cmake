961cc954d898b1595bfb42378869341dc5a9bcf2
Zhengiskan <64011433@kmitl.ac.th>
2021-11-16 15:05:42 +0700

    set up 2

---
+++ cmake-build-debug/CMakeFiles/Manipulating_TGA_Files.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Manipulating_TGA_Files.dir/Manipulating-tga.c.o"
+  "CMakeFiles/Manipulating_TGA_Files.dir/main.c.o"
+  "Manipulating_TGA_Files"
+  "Manipulating_TGA_Files.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Manipulating_TGA_Files.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
