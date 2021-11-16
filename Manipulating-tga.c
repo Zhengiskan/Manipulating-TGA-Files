04f6bd633f1c8b8671768de2ea8be5698dc13842
Zhengiskan <64011433@kmitl.ac.th>
2021-11-16 17:21:17 +0700

    set up 5

--- Manipulating-tga.c
+++ Manipulating-tga.c
@@ -3,10 +3,11 @@
 int main()
 {
     FILE *ptr;
-    ptr = fopen("sample1.tga", "rb");
+    ptr = fopen("sample1.tga", "wb");
     if (ptr == NULL){
         printf("ERROR, UNABLE TO OPEN FILE.");
         fclose(ptr);
     }
+
     return 0;
 }
