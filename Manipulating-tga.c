089d2749ff0a223ae6d555c5af7be84c70c94fcd
Zhengiskan <64011433@kmitl.ac.th>
2021-11-25 19:07:48 +0700

    importing library from git
    This is for reference only. After importing and using the library, some errors are shown which defies the point of using it for reference.
    
    Therefore, This is useless.

--- Manipulating-tga.c
+++ Manipulating-tga.c
@@ -2,12 +2,16 @@
 
 int main()
 {
+    char buffer[10];
     FILE *ptr;
     ptr = fopen("sample1.tga", "wb");
-    if (ptr == NULL){
+    if (ptr == NULL) {
         printf("ERROR, UNABLE TO OPEN FILE.");
         fclose(ptr);
     }
-    
+    fread(buffer,sizeof(buffer),1,ptr);
+    for(int i = 0; i<10; i++)
+        printf("%u ", buffer[i]);
+
     return 0;
 }
