de09578eb4165f4785983a1650ed3295fa015e63
Zhengiskan <64011433@kmitl.ac.th>
2021-12-12 16:09:23 +0700

    Given Cmakelist + Source code

---
+++ cmake-build-debug/CMakeFiles/Manipulating-TGA-Files.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Manipulating-TGA-Files.dir/Manipulating-tga.c.o"
+  "Manipulating-TGA-Files"
+  "Manipulating-TGA-Files.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Manipulating-TGA-Files.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
